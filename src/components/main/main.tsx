9d70b052746df872d6bc243d6dec63bd9c3e8164
Khalid <155519253+khalidh1b@users.noreply.github.com>
2025-03-13 16:23:00 +0600

    feat: removed uncessary console.log & logically sent type based searched data on card

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,37 +1,35 @@
 import { Card } from '@/components/ui/card';
 import { CardSkeleton } from '@/components/ui/card-skeleton';
 import { usePokemonContext } from '@/context/pokemonContext';
-import { useSearchPokemon } from '@/hooks/useSearchPokemon';
 
 export const Main = () => {
-    const { pokemons, loading, imgLoaded, setImgLoaded } = usePokemonContext();
-    const { searching } = useSearchPokemon();
+    const { pokemons, loading, imgLoaded, setImgLoaded, searching } = usePokemonContext();
 
     console.log(pokemons);
     console.log('pokemons');
+
     console.log('searchinghhhh',searching);
     console.log('imgloadeddd',imgLoaded);
-    console.log(loading);
+    console.log('loading', loading);
 
     return (
         <div className='grid grid-cols-4 gap-6 '>
                 
-            {loading || !searching || !imgLoaded && Array.from({ length: 12 }).map((_, idx) => (
+            {(loading) ? Array.from({ length: 12 }).map((_, idx) => (
                 <CardSkeleton key={idx}/>
-            ))}
-            
-            {   
+            ))  : 
                 pokemons.map((pokemon) => (
                     <Card 
-                        key={pokemon.id} 
-                        img={pokemon.sprites.other['official-artwork'].front_default}
-                        name={pokemon.name}
-                        rank={pokemon.id}
-                        types={pokemon.types}
+                        key={pokemon.id || pokemon[0].id} 
+                        img={pokemon.sprites?.other['official-artwork']?.front_default || pokemon[0].sprites?.other['official-artwork']?.front_default}
+                        name={pokemon.name || pokemon[0].name}
+                        rank={pokemon.id || pokemon[0].id}
+                        types={pokemon.types || pokemon[0].types}
                         setImgLoaded={setImgLoaded}
                     />
                 ))
             }
+            
         </div>
     )
 };
