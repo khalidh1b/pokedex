867d8dd61df4088c6fad37ed21169cee64aca50d
Khalid <155519253+khalidh1b@users.noreply.github.com>
2025-03-14 11:01:41 +0600

    feat: added theme component dark/white

---
+++ src/components/ui/theme.tsx
@@ -0,0 +1,16 @@
+import { Sun, Moon } from "lucide-react";
+import React from "react";
+
+interface ThemeProps {
+    theme: boolean; 
+    handleThemeChange: () => void; 
+};
+
+export const Theme: React.FC<ThemeProps> = ({ handleThemeChange, theme }) => {
+    
+    return (
+        <div className="flex cursor-pointer" onClick={() => handleThemeChange()}>
+            {theme ? <Moon/> : <Sun/>}
+        </div>
+    )
+};
