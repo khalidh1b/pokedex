59e780d67056d6b49bdcabe98ff6102d1fdd98b8
Khalid <155519253+khalidh1b@users.noreply.github.com>
2025-03-14 22:38:51 +0600

    refactor: break full component into smaller functional component and added proptypes

--- src/components/pokemon-detail/pokemon-detail.tsx
+++ src/components/pokemon-detail/pokemon-detail.tsx
@@ -1,26 +1,55 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { ArrowLeft, Flame } from "lucide-react";
 
-export const PokemonDetail = ({ closeModal }) => {
+type PokemonDetailProps = {
+  closeModal: () => void;
+  pokemon: any; 
+};
+
+export const PokemonDetail: React.FC<PokemonDetailProps> = ({ closeModal, pokemon }) => {
     const [activeTab, setActiveTab] = useState('about');
 
     return (
         <div className="flex items-center justify-center w-full min-h-screen p-4">
             <div className="w-full max-w-md rounded-3xl overflow-hidden bg-white shadow-lg">
-                <PokemonDetailHeader closeModal={closeModal}/>
+                <PokemonDetailHeader 
+                  closeModal={closeModal} 
+                  name={pokemon.name}
+                  order={pokemon.order}
+                  image={pokemon.sprites?.other['official-artwork']?.front_default}
+                />
                 <div className="bg-white rounded-t-3xl -mt-6 relative z-10">
                     <TabLists 
                         activeTab={activeTab} 
                         setActiveTab={setActiveTab}
                     />
-                    <TabContent activeTab={activeTab}/>
+                    <TabContent 
+                      activeTab={activeTab} 
+                      height={pokemon.height} 
+                      weight={pokemon.weight} 
+                      stats={pokemon.stats} 
+                      abilities={pokemon.abilities}
+                    />
                 </div>
             </div>
         </div>
     )
 };
 
-const PokemonDetailHeader = ({ closeModal }) => {
+type PokemonDetailHeaderProp = {
+  closeModal: () => void;
+  order: string;
+  name: string;
+  image: string
+};
+
+
+const PokemonDetailHeader: React.FC<PokemonDetailHeaderProp> = ({ 
+    closeModal, 
+    order, 
+    name, 
+    image 
+}) => {
     return (
         <div className="relative bg-[#fa7a7a] pt-4 pb-16">
 
@@ -39,7 +68,7 @@ const PokemonDetailHeader = ({ closeModal }) => {
 
               <div className="w-32 h-32 relative">
                 <img
-                  src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/details-72PCZpil1f5y4FoezXEhwIengJVD23.png"
+                  src={image}
                   alt="Charmeleon"
                   width={130}
                   height={130}
@@ -48,8 +77,8 @@ const PokemonDetailHeader = ({ closeModal }) => {
               </div>
 
             <div className="ml-2 text-white">
-                <p className="text-lg font-medium">#005</p>
-                <h1 className="text-4xl font-bold mb-2">Charmeleon</h1>
+                <p className="text-lg font-medium">#{order}</p>
+                <h1 className="text-4xl font-bold mb-2">{name}</h1>
                 <div className="inline-flex items-center px-3 py-1 rounded-full bg-[#ff7b00] bg-opacity-30">
                   <Flame size={16} className="mr-1" />
                   <span className="text-sm">Fire</span>
@@ -61,32 +90,60 @@ const PokemonDetailHeader = ({ closeModal }) => {
     )
 };
 
-const TabContent = ({ activeTab }) => {
+type TabContentProp = {
+  activeTab: string;
+  weight: string;
+  height: string;
+  stats: any;
+  abilities: any 
+};
+
+const TabContent: React.FC<TabContentProp> = ({ 
+  activeTab, 
+  weight, 
+  height, 
+  stats, 
+  abilities 
+}) => {
+    // console.log(stats);
+    console.log(abilities);
     return (
         <div className="p-6 pt-4">
             {activeTab === "about" && (
-              <div className="space-y-4">
+              <div className="space-y-4 text-[#212121]">
                 <div className="flex">
-                  <span className="w-24 font-medium text-gray-600">Species</span>
-                  <span>Fire</span>
+                  <span className="w-24 font-medium">Species</span>
+                  <span className="text-[#212121]">Fire</span>
                 </div>
                 <div className="flex">
-                  <span className="w-24 font-medium text-gray-600">Height</span>
-                  <span>110cm</span>
+                  <span className="w-24 font-medium ">Height</span>
+                  <span className="text-[#212121]">{height}cm</span>
                 </div>
                 <div className="flex">
-                  <span className="w-24 font-medium text-gray-600">Weight</span>
-                  <span>19kg</span>
+                  <span className="w-24 font-medium">Weight</span>
+                  <span className="text-[#212121]">{weight}kg</span>
                 </div>
                 <div className="flex">
-                  <span className="w-24 font-medium text-gray-600">Abilities</span>
-                  <span>Blaze, Solar Power</span>
+                  <span className="w-24 font-medium">Abilities</span>
+                  {abilities.map((ability: any, idx: number) => (
+                  <span key={idx} className="text-[#212121] pr-2">{ability.ability.name}</span>
+                  ))}
                 </div>
               </div>
             )}
 
             {activeTab === "base-stats" && (
-              <div className="text-center text-gray-500">Base stats content would go here</div>
+              stats.map((stat: any, idx: number) => (
+                <div key={idx} className="flex items-center justify-between mb-3">
+                  <div className="flex justify-between w-4/6 text-[#212121] text-base font-medium mr-7">
+                    <span>{stat.stat.name}</span>
+                    <span>{stat.base_stat}</span>
+                  </div>
+                  <div className="w-full bg-gray-300">
+                    <div className="h-1 rounded-md bg-[#FF6464]" style={{width: `${parseInt(stat.base_stat)}%`}}></div>
+                  </div>
+                </div>
+              ))
             )}
 
             {activeTab === "evolution" && (
@@ -96,7 +153,12 @@ const TabContent = ({ activeTab }) => {
     )
 };
 
-const TabLists = ({ activeTab, setActiveTab }) => {
+type TabListsProp = {
+  activeTab: string;
+  setActiveTab: React.Dispatch<React.SetStateAction<string>>; 
+};
+
+const TabLists: React.FC<TabListsProp> = ({ activeTab, setActiveTab }) => {
     return (
         <div className="flex border-b">
             <button
