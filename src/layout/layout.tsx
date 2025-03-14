4a27236677a8c911f03a9911dab18a1ee6b91c54
Khalid <155519253+khalidh1b@users.noreply.github.com>
2025-03-14 22:32:57 +0600

    refactor: moved main into routes and added outlet

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,13 +1,15 @@
 import { Header } from "@/components/header/header";
-import { Main } from "@/components/main/main";
 import { Toaster } from 'react-hot-toast';
+import { Outlet } from "react-router-dom";
 
 export const Layout = () => {
     return (
-        <div className="container mx-auto my-3 md:px-0 px-2">
-            <Toaster/>
-            <Header/>
-            <Main/>
+        <div className="dark:bg-gray-900 min-h-screen">
+            <div className="container mx-auto md:px-0 px-2">
+                <Toaster/>
+                <Header/>
+                <Outlet/>
+            </div>
         </div>
     )
 };
