18e4d7d69cb56f1d2bbb0e4bf858bb623618e790
Khalid <155519253+khalidh1b@users.noreply.github.com>
2025-03-14 10:57:28 +0600

    feat: added filtering feature using useHandleFilter() hook

---
+++ src/hooks/useHandleFilter.ts
@@ -0,0 +1,35 @@
+import { usePokemonContext } from '@/context/pokemonContext';
+import { useState } from 'react';
+
+const useHandleFilter = () => {
+    const [filterOpen, setFilterOpen] = useState(false);
+    const [filterTypes, setFilterTypes] = useState<string[]>([])
+    const { pokemons, setPokemon, fetchPokemon } = usePokemonContext();
+
+    const handleCheckboxChange = (type: string) => {
+        setFilterTypes((prev) => (
+            prev.includes(type) ? prev.filter((t) => t !== type) : [...prev, type]
+        ))
+    };
+
+    const filterPokemons = (types: string[]) => {
+        if(types.length === 0) {
+            setPokemon(pokemons);
+            return;
+        };
+
+        const filtered = pokemons.filter((pokemon) => {
+            return pokemon.types.some((t) => types.includes(t.type.name));
+        });
+        setPokemon(filtered);
+    };
+
+    const resetFilter = async () => {
+        await fetchPokemon();
+        setFilterTypes([]);
+    };
+    
+    return { filterPokemons, resetFilter, handleCheckboxChange, filterOpen, filterTypes, setFilterOpen }
+};
+
+export default useHandleFilter;
