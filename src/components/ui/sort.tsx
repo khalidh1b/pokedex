06358d35eb2910ccf898c0fe3fe76cdfaa7f160c
Khalid <155519253+khalidh1b@users.noreply.github.com>
2025-03-14 22:45:10 +0600

    feat: added sorting states on onChange handler, added proptypes and select option values

--- src/components/ui/sort.tsx
+++ src/components/ui/sort.tsx
@@ -1,10 +1,21 @@
-export const Sort = () => {
+import React from "react";
+
+interface SortProps {
+    setSortBy: React.Dispatch<React.SetStateAction<string>>;
+    sortBy: string;
+};
+
+export const Sort: React.FC<SortProps> = ({ setSortBy, sortBy }) => {
     return (
-        <select className="bg-white px-4 py-1 cursor-pointer focus:outline-none border-gray-300 border-2 rounded-md text-[#212121] font-medium">
-            <option value="">Lowest Number First</option>
-            <option value="">Highest Number First</option>
-            <option value="">Alphabetically (A-Z)</option>
-            <option value="">Alphabetically (Z-A)</option>
+        <select 
+            className="bg-white px-4 py-1 cursor-pointer focus:outline-none border-gray-300 border-2 rounded-md text-[#212121] font-medium"
+            onChange={(e) => setSortBy(e.target.value)}
+            value={sortBy}
+            >
+            <option value="id-asc">Lowest Number First</option>
+            <option value="id-desc">Highest Number First</option>
+            <option value="id-asc-alpha">Alphabetically (A-Z)</option>
+            <option value="id-desc-alpha">Alphabetically (Z-A)</option>
         </select>
     )
 };
