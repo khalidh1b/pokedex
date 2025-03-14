adda6df598532d29db48af183ce433af699d50ef
Khalid <155519253+khalidh1b@users.noreply.github.com>
2025-03-14 22:29:32 +0600

    feat: added infinite scrolling hook, card skeleton for imgLoading and cardDetails component conditionally

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,21 +1,42 @@
 import { Card } from '@/components/ui/card';
 import { CardSkeleton } from '@/components/ui/card-skeleton';
 import { usePokemonContext } from '@/context/pokemonContext';
+import useInfiniteScrolling from '@/hooks/useInfiniteScrolling';
+import { useState } from 'react';
+import { PokemonDetail } from '@/components/pokemon-detail/pokemon-detail';
 
 export const Main = () => {
-    const { pokemons, loading, imgLoaded, setImgLoaded, searching } = usePokemonContext();
+    const { pokemons, loading, searching, imgLoaded, setImgLoaded } = usePokemonContext();
+    const { ref, scrollLoading } = useInfiniteScrolling();
+    const [selectedPokemon, setSelectedPokemon] = useState(null);
 
-    console.log(pokemons);
-    console.log('pokemons');
+    const openModal = (pokemon: any) => {
+        setSelectedPokemon(pokemon);
+    };
 
-    console.log('searchinghhhh',searching);
-    console.log('imgloadeddd',imgLoaded);
-    console.log('loading', loading);
+    const closeModal = () => {
+        console.log('close modal')
+        setSelectedPokemon(null);
+    };
+
+    // console.log(loading, scrollLoading, searching)
+    // console.log('imgLoaded', imgLoaded);
+    // console.log('imgLoaded', setImgLoaded);
+    
+    const handleImgLoad = () => {
+        // console.log('handleimgload caliing')
+        setImgLoaded(true)
+    };
 
     return (
-        <div className='grid md:grid-cols-4 grid-cols-1 gap-6 '>
-                
-            {(loading) ? Array.from({ length: 12 }).map((_, idx) => (
+        <>
+        <div className='grid md:grid-cols-4 grid-cols-1 gap-6'>
+            
+            {!imgLoaded && Array.from({ length: 12 }).map((_, idx) => (
+                <CardSkeleton key={idx}/>
+            ))}
+
+            {(loading || scrollLoading || searching) ? Array.from({ length: 12 }).map((_, idx) => (
                 <CardSkeleton key={idx}/>
             ))  : 
                 pokemons.map((pokemon) => {
@@ -28,12 +49,21 @@ export const Main = () => {
                             name={currentPokemon.name}
                             rank={currentPokemon.id}
                             types={currentPokemon.types}
-                            setImgLoaded={setImgLoaded}
+                            openModal={openModal}
+                            pokemon={pokemon}
+                            handleImgLoad={handleImgLoad}
                         />
                     )
                 })
             }
-            
-        </div>
+
+            {selectedPokemon && (
+                <div className="fixed inset-0 bg-opacity-50 flex justify-center items-center">
+                  <PokemonDetail closeModal={closeModal} pokemon={selectedPokemon}/>
+              </div>
+            )}
+          </div>
+          <div ref={ref}></div>
+        </>
     )
 };
