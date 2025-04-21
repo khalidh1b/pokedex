7fe9e3428532b3788bac022f966f055dc95183b1
Khalid <155519253+khalidh1b@users.noreply.github.com>
2025-04-21 17:07:48 +0600

    refactor: removed inline arrow function from onclick handler in the Theme component

--- src/components/ui/theme.tsx
+++ src/components/ui/theme.tsx
@@ -9,7 +9,7 @@ interface ThemeProps {
 export const Theme: React.FC<ThemeProps> = ({ handleThemeChange, theme }) => {
     
     return (
-        <div className="flex cursor-pointer" onClick={() => handleThemeChange()}>
+        <div className="flex cursor-pointer" onClick={handleThemeChange}>
             {theme ? <Moon/> : <Sun/>}
         </div>
     )
