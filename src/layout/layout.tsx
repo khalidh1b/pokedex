e5ad2b157ea2877d2e6d18f86c8351d7ce144494
Khalid <155519253+khalidh1b@users.noreply.github.com>
2025-10-27 09:29:43 +0600

    ..

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -5,8 +5,8 @@ import { Outlet } from "react-router-dom";
 export const Layout = () => {
     return (
         <div className="dark:bg-gray-900 min-h-screen">
+            <Toaster/>
             <div className="container mx-auto md:px-0 px-2">
-                <Toaster/>
                 <Header/>
                 <Outlet/>
             </div>
