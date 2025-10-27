a6359e35fcdd23d37e3d9bf8e0597b6c9ffcf093
Khalid <155519253+khalidh1b@users.noreply.github.com>
2025-10-27 09:33:53 +0600

    chore: removed pokemon-not-found route

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,5 +1,4 @@
 import { Main } from '@/components/main/main';
-import { PokemonNotFound } from '@/components/pokemon-not-found/pokemon-not-found';
 import { Layout } from '@/layout/layout';
 import { createBrowserRouter } from 'react-router-dom';
 
@@ -11,10 +10,6 @@ export const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Main/>
-            },
-            {
-                path: '/pokemon-not-found',
-                element: <PokemonNotFound/>
             }
         ]
     }
